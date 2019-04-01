a74c1d3794fc7203d63246748367ddb3a0f95022
Jenny Chu <jchudesign0930@gmail.com>
2019-04-01 02:50:19 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,16 @@
+class Author
+  attr_accessor :name, :posts
+  
+  def initialize (name)
+    @name = name
+  end
+  
+  def posts
+    @posts = posts
+  end
+  
+  def add_post(new_post)
+    
+  end
+  
+end
