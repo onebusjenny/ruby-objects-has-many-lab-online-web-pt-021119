30b7d4f7cf1c195c807cab5fedf771bd299c9586
Jenny Chu <jchudesign0930@gmail.com>
2019-04-01 02:33:42 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,23 @@
+class Artist
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+ 
+  def add_song(song)
+    @songs << song
+    song.artist= self
+  end
+ 
+  def add_song_by_name(song_name,artist)
+    song = song.new(song_name,artist)
+    @songs << song
+    song.artist = self
+  end
+  
+  def songs
+    @songs
+  end
+end
