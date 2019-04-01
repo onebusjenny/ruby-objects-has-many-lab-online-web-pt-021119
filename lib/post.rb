c2156e47aa0c0b04596fc11dc0f22b4b7b215c79
Jenny Chu <jchudesign0930@gmail.com>
2019-04-01 03:03:08 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,7 +1,7 @@
 class Post
-  attr_accessor :title
+  attr_accessor :title, :author
   
-  def initialize (title)
+  def initialize(title)
     @title = title
     @@all << self
   end
@@ -10,11 +10,23 @@ class Post
     @@all
   end
   
-  def title(new_title)
-    self.title.new_title
-  end
-  
   def author 
     self.author.name
+  end
+end
+
+class Post
+
+  attr_accessor :title, :author
+  def initialize(title)
+    @title = title
+  end
+
+  def author_name
+    if @author == nil
+      return nil
+    else
+      @author.name
+    end
   end
 end
